5cebdae566f43ecaeb8cb437c0c18731dfc474fa
RaulCuart <raulcuartortega@a.cifpperedesongall.org>
2025-01-27 16:34:57 +0100

    Comentarios y api.php corregido

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
+// Funcionalidad Logout con el controller de AuthController 'logout'
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+//Funcionalidad Register
 Route::post('/register', [AuthController::class, 'register']);
 
+//Funcionalidad Login
 Route::post('/login', [AuthController::class, 'login']);
